68ae28f2009a889ff0e2d7f6113b56e252841ca9
Celso Blackfyre <celsomartinscarvalho@gmail.com>
2024-05-15 04:02:05 -0300

    mimir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { CadastroForm } from "../../components/conta/Registrar";
 import { Login } from "../../components/conta/Login";
+import { Modal } from "../../components/alimentacao/modal";
 
 export default function Home() {
 	return (
 		<>
+			<Modal />
 			<Login />
 			<CadastroForm />
 		</>
