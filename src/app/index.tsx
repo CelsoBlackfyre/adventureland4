b108ca44bd5798bcc938409c64052db7ef5beebf
Celso Blackfyre <celsomartinscarvalho@gmail.com>
2024-05-16 04:17:31 -0300

    mongo teste

---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+// pages/index.tsx
+
+import { connectToDatabase } from "../lib/mongodb";
+
+export default function Home() {
+	// Example usage of connectToDatabase
+	async function fetchData() {
+		const { db } = await connectToDatabase();
+		const collection = db.collection("myCollection");
+		const data = await collection.find().toArray();
+		console.log(data);
+	}
+
+	fetchData();
+
+	return (
+		<div>
+			<h1>Hello Next.js</h1>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
 	return (
 		<>
 			{/* <MeuFormulario /> */}
+			<Home />
 			<AppHeader />
 			<Loja />
 			<CadastroForm />
