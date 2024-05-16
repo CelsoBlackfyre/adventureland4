45d392bfd86b35a70cba676592f149b1509ae5d8
Celso Blackfyre <celsomartinscarvalho@gmail.com>
2024-05-16 03:28:05 -0300

    madruga

---
+++ components/DadosUsuario/dadosUsuario.tsx
@@ -0,0 +1,12 @@
+export interface FormData {
+	nomeUsuario: string;
+	nome: string;
+	email: string;
+	idade: number;
+	sexo: string;
+	senha: string;
+	confirmarSenha: string;
+	cidade: string;
+	estado: string;
+	erroMensagem: string;
+}
