93af6d0ab606d0f30f50f91e75dde287a88296a2
Celso Blackfyre <celsomartinscarvalho@gmail.com>
2024-05-16 16:03:30 -0300

    tarde

--- components/carrossel/carrossel.tsx
+++ components/carrossel/carrossel.tsx
@@ -1,47 +1,96 @@
 import React from "react";
+import Image from "next/image";
+import { TECarousel, TECarouselItem } from "tw-elements-react";
 import sakurahouse from "../../src/imagens/sakurahouse.jpg";
 import park3 from "../../src/imagens/japanesevillage.jpg";
 import park4 from "../../src/imagens/brinquedo6.png";
-import { Carousel } from "react-bootstrap";
-import "./carrossel.css";
 
 function CarouselB() {
 	return (
-		<Carousel className="carousel-container font-loader" data-bs-theme="dark">
-			<div className="carousel-item carousel-image">
-				<img className="d-block w-100" src={sakurahouse} alt="First slide" />
-				<div className="carousel-caption">
-					<h5 className="text-white display-5 fw-bold">
-						<span> SEJA BEM-VINDO AO NOSSO PARQUE</span>
-					</h5>
-					<p className="text-white fs-3">
-						<span>私たちの公園へようこそ</span>
-					</p>
-				</div>
-			</div>
-			<div className="carousel-item carousel-image">
-				<img className="d-block w-100" src={park3} alt="Second slide" />
-				<div className="carousel-caption">
-					<h5 className="text-white display-5 fw-bold">
-						<span> O MUNDO FANTASTICO DA CULTURA JAPONESA</span>
-					</h5>
-					<p className="text-white fs-3">
-						<span>幻想的な日本文化の世界</span>
-					</p>
-				</div>
-			</div>
-			<div className="carousel-item carousel-image">
-				<img className="d-block w-100" src={park4} alt="Third slide" />
-				<div className="carousel-caption">
-					<h5 className="text-white display-5 fw-bold">
-						<span>VENHA CONHECER NOSSAS ATRACOES</span>
-					</h5>
-					<p className="text-white fs-3">
-						<span>私たちのアトラクションを見に来てください</span>
-					</p>
-				</div>
-			</div>
-		</Carousel>
+
+
+
+		<>
+		<TECarousel showControls showIndicators crossfade ride="carousel">
+		  <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+			<TECarouselItem
+			  itemID={1}
+			  className="relative float-left -mr-[100%] hidden w-full !transform-none transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
+			>
+			  <img
+				src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
+				className="block w-full"
+				alt="..."
+			  />
+			</TECarouselItem>
+			<TECarouselItem
+			  itemID={2}
+			  className="relative float-left -mr-[100%] hidden w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
+			>
+			  <img
+				src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+				className="block w-full"
+				alt="..."
+			  />
+			</TECarouselItem>
+			<TECarouselItem
+			  itemID={3}
+			  className="relative float-left -mr-[100%] hidden w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
+			>
+			  <img
+				src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
+				className="block w-full"
+				alt="..."
+			  />
+			</TECarouselItem>
+		  </div>
+		</TECarousel>
+	  </>
+	);
+  }
+
+
+
+
+
+
+
+
+		// <Carousel className="carousel-container font-loader" data-bs-theme="dark">
+		// 	<div className="carousel-item carousel-image">
+		// 		<Image className="d-block w-100" src={sakurahouse} alt="First slide" />
+		// 		<div className="carousel-caption">
+		// 			<h5 className="text-white display-5 fw-bold">
+		// 				<span> SEJA BEM-VINDO AO NOSSO PARQUE</span>
+		// 			</h5>
+		// 			<p className="text-white fs-3">
+		// 				<span>私たちの公園へようこそ</span>
+		// 			</p>
+		// 		</div>
+		// 	</div>
+		// 	<div className="carousel-item carousel-image">
+		// 		<Image className="d-block w-100" src={park3} alt="Second slide" />
+		// 		<div className="carousel-caption">
+		// 			<h5 className="text-white display-5 fw-bold">
+		// 				<span> O MUNDO FANTASTICO DA CULTURA JAPONESA</span>
+		// 			</h5>
+		// 			<p className="text-white fs-3">
+		// 				<span>幻想的な日本文化の世界</span>
+		// 			</p>
+		// 		</div>
+		// 	</div>
+		// 	<div className="carousel-item carousel-image">
+		// 		<Image className="d-block w-100" src={park4} alt="Third slide" />
+		// 		<div className="carousel-caption">
+		// 			<h5 className="text-white display-5 fw-bold">
+		// 				<span>VENHA CONHECER NOSSAS ATRACOES</span>
+		// 			</h5>
+		// 			<p className="text-white fs-3">
+		// 				<span>私たちのアトラクションを見に来てください</span>
+		// 			</p>
+		// 		</div>
+		// 	</div>
+		// </Carousel>
 	);
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,12 @@ import CadastroForm from "../../components/conta/Registrar";
 import Loja from "../../components/loja/loja";
 import AppHeader from "../../components/header/navbar";
 import Footer from "../../components/footer/footer";
+import CarouselB from "../../components/carrossel/carrossel";
 export default function Home() {
 	return (
 		<>
 			{/* <MeuFormulario /> */}
+			<CarouselB />
 			<AppHeader />
 			<Loja />
 			<CadastroForm />
